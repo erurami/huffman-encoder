f7526376c00dac59b0f7d5d55e6b5e6dbdd3464b
erurami <88187787+erurami@users.noreply.github.com>
2022-01-05 18:00:02 +0700

    compression wizard UIs.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@
 
 #include <Windows.h>
 #include <Commctrl.h>
+#include <shlwapi.h>
 
 #include "libs\libs\huffman.hpp"
 
@@ -25,6 +26,7 @@
 #pragma comment(lib,"User32.lib")
 #pragma comment(lib,"gdi32.lib")
 #pragma comment(lib, "Comdlg32.lib")
+#pragma comment(lib, "Shlwapi.lib")
 
 // thanks microsoft docs
 #pragma comment(linker,"\"/manifestdependency:type='win32' \
@@ -451,7 +453,7 @@ HPEN DrawArrowCompression(HDC hdc)
 
 
 #define PROGRESS_WND_WIDTH  400
-#define PROGRESS_WND_HEIGHT 400
+#define PROGRESS_WND_HEIGHT 220
 
 LRESULT CALLBACK CompressionGuiProc(
         HWND hWnd,
@@ -485,10 +487,11 @@ bool StartCompressionWithGUI(CompressionInfos* pCompInfo)
 
 
 
+
     HWND hProgressWnd;
     hProgressWnd = CreateWindow(
             TEXT("CompressionProgress"), TEXT("Compressing..."),
-            WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+            WS_TILED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
             CW_USEDEFAULT, CW_USEDEFAULT,
             PROGRESS_WND_WIDTH, PROGRESS_WND_HEIGHT,
             NULL, NULL, G_H_Instance, NULL);
@@ -513,14 +516,19 @@ bool StartCompressionWithGUI(CompressionInfos* pCompInfo)
 
 
 
+
 #define COMPRESSION_END_CODE_SUCCESS         1
 #define COMPRESSION_END_CODE_ERROR           2
 #define COMPRESSION_END_CODE_FAILED_OPENFILE 3
+#define COMPRESSION_END_CODE_USER_CANCEL     4
 
 DWORD WINAPI CompressThreadFunc(LPVOID vdParam);
 
 #define TIMER_ID_UPDATE_PROGRESS 1
 #define TIMER_ID_GET_THREAD_STATE 2
+
+#define BUTTON_ID_CANCEL    1
+#define BUTTON_ID_PLAYPAUSE 2
 
 LRESULT CALLBACK CompressionGuiProc(
         HWND hWnd,
@@ -531,12 +539,29 @@ LRESULT CALLBACK CompressionGuiProc(
 
     static CompressionInfos comp_infos;
 
+    static TCHAR str_compression_description[MAX_PATH * 2 + 50];
+    static TCHAR str_progress[100];
+    static TCHAR str_compression_stage[100];
+
 
     static long n_progress_percentage;
     static int  n_compression_stage;
+    static int  n_compression_stage_last_time;
+
+    static bool bl_is_paused;
 
 
     static HWND h_progress_bar;
+
+    static HWND h_button_cancel;
+    static HWND h_button_playpause;
+
+
+    static HFONT h_font_description;
+    static HFONT h_font_progress;
+    static HFONT h_font_compression_stage;
+
+
     static HANDLE h_compression_thread;
 
     static DWORD dw_thread_id;
@@ -552,30 +577,85 @@ LRESULT CALLBACK CompressionGuiProc(
         case WM_CLOSE:
             if (MessageBox(hWnd, TEXT("cancel?"), TEXT("message"), MB_YESNO | MB_ICONINFORMATION) == IDYES)
             {
-                // some canceling process e.g. delete files..
-                DestroyWindow(hWnd);
-                PostQuitMessage(0);
+                // TODO : some canceling process e.g. delete files..
+                TerminateThread(h_compression_thread, COMPRESSION_END_CODE_USER_CANCEL);
             }
             return 0;
 
 
 
         case WM_CREATE:
+            {
 
-            h_progress_bar = CreateWindow(
-                    PROGRESS_CLASS, TEXT("Progress"),
-                    WS_CHILD | WS_VISIBLE ,
-                    10, 10, 300, 30,
-                    hWnd, NULL, G_H_Instance, NULL);
+                bl_is_paused = false;
 
 
-            PostMessage(h_progress_bar, PBM_SETRANGE, 0, MAKEWPARAM(0, 100));
+                h_font_description = CreateFont(
+                    20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+                    SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS,
+                    CLIP_DEFAULT_PRECIS, PROOF_QUALITY,
+                    VARIABLE_PITCH | FF_MODERN, TEXT("Segoe UI"));
+
+                h_font_progress = CreateFont(
+                    32, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+                    SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS,
+                    CLIP_DEFAULT_PRECIS, PROOF_QUALITY,
+                    VARIABLE_PITCH | FF_MODERN, TEXT("Segoe UI"));
+
+                h_font_compression_stage = CreateFont(
+                    20, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE,
+                    SHIFTJIS_CHARSET, OUT_DEFAULT_PRECIS,
+                    CLIP_DEFAULT_PRECIS, PROOF_QUALITY,
+                    VARIABLE_PITCH | FF_MODERN, TEXT("Segoe UI"));
 
 
-            //                                        Å´updating progress frequency
-            SetTimer(hWnd, TIMER_ID_UPDATE_PROGRESS , 10, NULL);
-            SetTimer(hWnd, TIMER_ID_GET_THREAD_STATE, 100, NULL);
 
+                RECT rectClientArea;
+
+                GetClientRect(hWnd, &rectClientArea);
+
+                int window_width  = rectClientArea.right  - rectClientArea.left;
+                int window_height = rectClientArea.bottom - rectClientArea.top;
+
+
+                h_progress_bar = CreateWindow(
+                        PROGRESS_CLASS, TEXT("Progress"),
+                        WS_CHILD | WS_VISIBLE,
+                        10, 75, window_width - 20, 25,
+                        hWnd, NULL, G_H_Instance, NULL);
+
+
+                CreateWindow(
+                        TEXT("STATIC"), TEXT(""),
+                        WS_CHILD | WS_VISIBLE,
+                        0, 125, window_width, window_height - 125,
+                        hWnd, NULL, G_H_Instance, NULL);
+
+                h_button_cancel = CreateWindow(
+                        TEXT("BUTTON"), TEXT("Cancel"),
+                        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                        window_width - 110, 140, 100, 25,
+                        hWnd, (HMENU)BUTTON_ID_CANCEL, G_H_Instance, NULL);
+
+                h_button_playpause = CreateWindow(
+                        TEXT("BUTTON"), TEXT("Pause"),
+                        WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+                        window_width - 220, 140, 100, 25,
+                        hWnd, (HMENU)BUTTON_ID_PLAYPAUSE, G_H_Instance, NULL);
+
+
+                SendMessage(h_button_cancel   , WM_SETFONT, (WPARAM)h_font_compression_stage, TRUE);
+                SendMessage(h_button_playpause, WM_SETFONT, (WPARAM)h_font_compression_stage, TRUE);
+
+
+                PostMessage(h_progress_bar, PBM_SETRANGE, 0, MAKEWPARAM(0, 100));
+
+
+
+                //                                        Å´updating progress frequency
+                SetTimer(hWnd, TIMER_ID_UPDATE_PROGRESS , 10, NULL);
+                SetTimer(hWnd, TIMER_ID_GET_THREAD_STATE, 100, NULL);
+            }
             return 0;
 
 
@@ -585,7 +665,46 @@ LRESULT CALLBACK CompressionGuiProc(
             {
 
                 case TIMER_ID_UPDATE_PROGRESS:
+
                     PostMessage(h_progress_bar, PBM_SETPOS, n_progress_percentage, 0);
+
+                    wsprintf(str_progress, TEXT("%d%% complete"), n_progress_percentage);
+
+                    if (n_compression_stage != n_compression_stage_last_time)
+                    {
+                        switch(n_compression_stage)
+                        {
+
+                            case COMPRESSION_STEP_READFREQ:
+                                lstrcpy(str_compression_stage, TEXT("Reading file"));
+                                break;
+
+                            case COMPRESSION_STEP_BUILDTREE:
+                                lstrcpy(str_compression_stage, TEXT("Building tree"));
+                                break;
+
+                            case COMPRESSION_STEP_WRITEINFO1:
+                                lstrcpy(str_compression_stage, TEXT("Writing header infos"));
+                                break;
+
+                            case COMPRESSION_STEP_ENCODE:
+                                lstrcpy(str_compression_stage, TEXT("Encoding file"));
+                                break;
+
+                            case COMPRESSION_STEP_WRITEINFO2:
+                                lstrcpy(str_compression_stage, TEXT("Writing file infos"));
+                                break;
+
+                            case COMPRESSION_STEP_FINISHED:
+                                lstrcpy(str_compression_stage, TEXT("finished"));
+                                break;
+
+                        }
+                        InvalidateRect(hWnd, NULL, TRUE);
+                        n_compression_stage_last_time = n_compression_stage;
+                    }
+
+                    InvalidateRect(hWnd, NULL, FALSE);
                     break;
 
                 case TIMER_ID_GET_THREAD_STATE:
@@ -609,23 +728,29 @@ LRESULT CALLBACK CompressionGuiProc(
                         {
 
                             case COMPRESSION_END_CODE_SUCCESS:
-                                MessageBox(NULL, TEXT("compression finished"), TEXT(""), MB_ICONINFORMATION);
+                                MessageBox(hWnd, TEXT("compression finished"), TEXT(""), MB_ICONINFORMATION);
                                 break;
 
                             case COMPRESSION_END_CODE_ERROR:
-                                MessageBox(NULL, TEXT("an error ocuured"), TEXT(""), MB_ICONINFORMATION);
+                                MessageBox(hWnd, TEXT("an error ocuured"), TEXT(""), MB_ICONINFORMATION);
                                 break;
 
                             case COMPRESSION_END_CODE_FAILED_OPENFILE:
-                                MessageBox(NULL, TEXT("failed to open file"), TEXT(""), MB_ICONINFORMATION);
+                                MessageBox(hWnd, TEXT("failed to open file"), TEXT(""), MB_ICONINFORMATION);
+                                break;
+
+                            case COMPRESSION_END_CODE_USER_CANCEL:
                                 break;
 
                             default:
-                                MessageBox(NULL, TEXT("finifhed with unexpected finish code"), TEXT(""), MB_ICONINFORMATION);
+                                MessageBox(hWnd, TEXT("finifhed with unexpected finish code"), TEXT(""), MB_ICONINFORMATION);
                                 break;
 
                         }
 
+                        DeleteObject(h_font_description);
+                        DeleteObject(h_font_progress);
+                        DeleteObject(h_font_compression_stage);
                         DestroyWindow(hWnd);
                         PostQuitMessage(0);
                     }
@@ -645,7 +770,13 @@ LRESULT CALLBACK CompressionGuiProc(
 
                 h_compression_thread = CreateThread(NULL, 0, CompressThreadFunc, (LPVOID)&comp_infos, 0, &dw_thread_id);
 
-                InvalidateRect(hWnd, NULL, TRUE);
+
+                wsprintf(str_compression_description,
+                         TEXT("compressing from %s to %s"),
+                         PathFindFileName(comp_infos.strFrom),
+                         PathFindFileName(comp_infos.strTo));
+
+                InvalidateRect(hWnd, NULL, FALSE);
             }
             return 0;
 
@@ -659,19 +790,48 @@ LRESULT CALLBACK CompressionGuiProc(
 
 
 
-        case WM_PAINT:
+        case WM_COMMAND:
+            switch (LOWORD(wp))
             {
 
+                case BUTTON_ID_CANCEL:
+                    PostMessage(hWnd, WM_CLOSE, 0, 0);
+                    break;
+
+                case BUTTON_ID_PLAYPAUSE:
+                    if (bl_is_paused == true)
+                    {
+                        ResumeThread(h_compression_thread);
+                        SetWindowText(h_button_playpause, TEXT("Pause"));
+                    }
+                    else
+                    {
+                        SuspendThread(h_compression_thread);
+                        SetWindowText(h_button_playpause, TEXT("Play"));
+                    }
+                    bl_is_paused = !bl_is_paused;
+                    break;
+
+            }
+            return 0;
+
+
+
+        case WM_PAINT:
+            {
                 HDC hdc;
                 PAINTSTRUCT ps;
 
                 hdc = BeginPaint(hWnd, &ps);
 
-                // TextOut(hdc, 10, 10, comp_info.strFrom, lstrlen(comp_info.strFrom));
+                SelectObject(hdc, h_font_description);
+                TextOut(hdc, 20, 10, str_compression_description, lstrlen(str_compression_description));
 
-                // TextOut(hdc, 10, 30, TEXT("to")       , 2);
+                SelectObject(hdc, h_font_progress);
+                TextOut(hdc, 20, 30, str_progress, lstrlen(str_progress));
 
-                // TextOut(hdc, 10, 50, comp_info.strTo  , lstrlen(comp_info.strTo  ));
+                SelectObject(hdc, h_font_compression_stage);
+                TextOut(hdc, 20, 100, str_compression_stage, lstrlen(str_compression_stage));
 
                 EndPaint(hWnd, &ps);
 
@@ -686,6 +846,9 @@ LRESULT CALLBACK CompressionGuiProc(
 }
 #undef TIMER_ID_UPDATE_PROGRESS
 
+#undef BUTTON_ID_CANCEL
+#undef BUTTON_ID_PLAYPAUSE
+
 struct ProgressUpdateInfos
 {
     int * npStage;
@@ -694,6 +857,7 @@ struct ProgressUpdateInfos
 
 void CallbackForCompression(long progress, int stage, void* param);
 
+// TODO : delete original file if the keeporiginal checkbox unchecked.
 DWORD WINAPI CompressThreadFunc(LPVOID vdParam)
 {
     CompressionInfos comp_infos;
@@ -705,6 +869,12 @@ DWORD WINAPI CompressThreadFunc(LPVOID vdParam)
     prog_update_infos.npProgress = comp_infos.npProgress;
 
 
+    // Dummy (for debug)
+    // for (int i = 0; i < 101; i++)
+    // {
+    //     CallbackForCompression(i, i / 20 + 1, (void*)&prog_update_infos);
+    //     Sleep(100);
+    // }
 
     FILE* p_file1;
     FILE* p_file2;
