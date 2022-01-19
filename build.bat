dce9df23cd5fe433d257d7a1df4f5e1ba478dde0
erurami <88187787+erurami@users.noreply.github.com>
2022-01-19 19:00:34 +0700

    improve build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,45 @@
 
-rc GUI\\resources\\resource.rc
+@echo off
 
 
-cl main.cpp GUI\\resources\\resource.res /W4
+call rc GUI\\resources\\resource.rc
+
+if "%errorlevel%" == "9009" (
+    echo rc not installed
+    goto EOF
+)
+
+if errorlevel 1 (
+    goto EOF
+)
 
 
-mt.exe -manifest main.exe.manifest -outputresource:main.exe;1
+call cl main.cpp GUI\\resources\\resource.res /W4
+
+if "%errorlevel%" == "9009" (
+    echo cl not installed
+    goto EOF
+)
+
+if errorlevel 1 (
+    goto EOF
+)
+
+
+call mt.exe -manifest main.exe.manifest -outputresource:main.exe;1
+
+if "%errorlevel%" == "9009" (
+    echo mt not installed
+    goto EOF
+)
+
+if errorlevel 1 (
+    goto EOF
+)
 
 del GUI\\resources\\resource.res
 del main.exe.manifest
 del main.obj
+
+:EOF
 
